c2d085f4a0c00c809f09f8b35155da14f67a92f8
davidhamp <david.hamp@gmail.com>
2015-04-20 10:25:57 -0700

    Tweaks
    Wrong namespace type added to loader

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,6 @@ define('__BASE__', __DIR__);
 define('__PROJECT_NAMESPACE__', 'ResumeService');
 
 $loader = require 'vendor/autoload.php';
-$loader->add(__PROJECT_NAMESPACE__, __BASE__ . '/src');
+$loader->addPsr4(__PROJECT_NAMESPACE__ . '\\', __BASE__ . '/Resume');
 
 $application = new SPF\Application();
